beb784b24aba105ab943e9f4b97331a1ebf99ae0
erich_wittenbeck <erich.wittenbeck@gmx.net>
2019-09-02 00:11:37 +0200

    - .....

--- scripts/experiments/feature_selection/feature_selection_full_set.py
+++ scripts/experiments/feature_selection/feature_selection_full_set.py
@@ -1,32 +1,45 @@
-
 from api import *
 
 history = Chart('C:/Users/User/Desktop/darmstadt/Master-Thesis/Data/Histories/btc_usd_jan2012-jun2018.csv') \
     .fill_missing_data('min', 'interpolate')\
     .upscale('60min')
 
-# TODO Indicators!!!
-
-macd = Indicator('macd', 'macd_main_line', 'macd_signal_line')\
-        .transform(lambda main_line, signal_line: signal_line - main_line, 'macd')
-trix = Indicator('trix', 'trix_main_line', 'trix_signal_line')\
-        .transform(lambda main_line, signal_line: signal_line - main_line, 'trix')
-adx = Indicator('adx', 'adx')
-rsi = Indicator('rsi', 'rsi')
-mfi = Indicator('mfi', 'mfi')
-wpr = Indicator('wpr', 'wpr')
-ao = Indicator('ao', 'ao')
-uo = Indicator('uo', 'uo')
-stoch = Indicator('stoch', 'stoch_k', 'stoch_d')
-bbands = Indicator('bbands', 'bb_u', 'bb_m', 'bb_l')\
-        .transform(lambda upper, middle, lower: upper - lower, 'bbands')
-kltch = Indicator('kltch', 'klt_u', 'klt_m', 'klt_l')\
-        .transform(lambda upper, middle, lower: upper - lower, 'kltch')
-
 features = Features(history) \
-    .label_data('ternary', 1, 0, 0)\
-    .add_features(macd, trix, adx, rsi, mfi, wpr, ao, uo, stoch, bbands, kltch)\
-    .prune_features(5)
+        .label_data('ternary', 1, 0, 0)\
+        .add_features(Indicator('macd', 'macd'),
+                      Indicator('trix', 'trix'),
+                      Indicator('adx', 'adx'),
+                      Indicator('vrtx', 'vrtx'),
+                      Indicator('rsi', 'rsi'),
+                      Indicator('mfi', 'mfi'),
+                      Indicator('wpr', 'wpr'),
+                      Indicator('ao', 'ao'),
+                      Indicator('uo', 'uo'),
+                      Indicator('stock_k', 'stoch', return_as='k'),
+                      Indicator('stoch_d', 'stoch', return_as='d'),
+                      Indicator('midx', 'midx'),
+                      Indicator('bbands_total', 'bbands', return_as='total_delta'),
+                      Indicator('bbands_upper', 'bbands', return_as='upper_delta'),
+                      Indicator('bbands_lower', 'bbands', return_as='lower_delta'),
+                      Indicator('kltch_total', 'kltch', return_as='total_delta'),
+                      Indicator('kltch_upper', 'kltch', return_as='upper_delta'),
+                      Indicator('kltch_lower', 'kltch', return_as='lower_delta'),
+                      Indicator('obv', 'obv'),
+                      Indicator('fidx', 'fidx'),
+                      Indicator('cmf', 'cmf'),
+                      Indicator('adl', 'adl'))
 
-print(features.X.columns)
+spans = ['year', 'quarter', 'month', 'week']
+selected_features = {feature: {span: 0 for span in spans} for feature in features.X.columns}
 
+for span in spans:
+    matrices = features.split(span)
+    increment = 1/len(matrices)
+
+    for matrix in matrices:
+        matrix.prune_features(5)
+
+        for feature in matrix.X.columns:
+            selected_features[feature][span] += increment
+
+print(selected_features)
