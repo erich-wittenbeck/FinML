b7e0ec20728b03301837b561cb0a3da23c754bf2
erich_wittenbeck <erich.wittenbeck@gmx.net>
2020-02-28 14:15:46 +0100

    - Add visual logreg rfe

--- scripts/experiments/feature_selection/feature_selection_rfe_logreg_visual.py
+++ scripts/experiments/feature_selection/feature_selection_rfe_logreg_visual.py
@@ -1,12 +1,11 @@
 
 from api import *
-from lib.indicators.dummy import random_standard_variable as baseline
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-# history = Chart('/home/files/charts/btc_usd_jan2012-jun2018.csv') \
-history = Chart('C:/Users/User/Desktop/darmstadt/Master-Thesis/Data/Histories/btc_usd_jan2012-jun2018.csv') \
+#history = Chart('C:/Users/User/Desktop/darmstadt/Master-Thesis/Data/Histories/btc_usd_jan2012-jun2018.csv') \
+history = Chart('/home/files/charts/btc_usd_jan2012-jun2018.csv') \
     .fill_missing_data('min', 'interpolate')\
     .upscale('60min')
 
@@ -33,8 +32,7 @@ features = Features(history) \
                         Indicator('obv', 'obv'),
                         Indicator('fidx', 'fidx'),
                         Indicator('cmf', 'cmf'),
-                        Indicator('adl', 'adl'),
-                        Indicator('baseline', baseline))
+                        Indicator('adl', 'adl'))
 
 spans = ['year', 'quarter', 'month', 'week']
 n = {'year': 1, 'quarter': 4, 'month': 12, 'week': 52}
@@ -44,20 +42,28 @@ for span in spans:
     date_times = []
 
     matrices = features.split(span)
-    num_of_matrices = len(matrices)
 
     for matrix in matrices:
-        matrix.standardize()\
-              .prune_features('baseline')
+        training_data, test_data = matrix.split(0.75)
+        training_data.standardize()
+        test_data.standardize(training_data)
 
-        selected_features = matrix.X.columns
+        try:
+            logreg = Classification('logreg', 'logreg')\
+                .set_hyper_parameters(C=[10**exp for exp in range(-6, 4)]) \
+                .configure_hpo('exhaustive', 'f1_macro', n_jobs=3, verbose=2) \
+                .train(training_data, prune_features=True, rfe_scoring='f1_macro')
 
-        for feature in selection_timelines:
-            selection_timelines[feature] += [1 if feature in selected_features else 0]
+            for feature in selection_timelines:
+                selection_timelines[feature] += [1 if feature in logreg.features else 0]
+        except ValueError:
+            for feature in selection_timelines:
+                selection_timelines[feature] += [0]
 
         date_times += [matrix.X.index[-1]]
 
-    selection_timelines = {feature: selection_timelines[feature] for feature in selection_timelines if not all(v == 0 for v in selection_timelines[feature])}
+    selection_timelines = {feature: selection_timelines[feature] for feature in selection_timelines if
+                           not all(v == 0 for v in selection_timelines[feature])}
     selection_timelines["date"] = [date_time.strftime('%Y-%m-%d') for date_time in date_times]
     df = pd.DataFrame.from_dict(selection_timelines)
     df = df.set_index("date")
@@ -68,15 +74,11 @@ for span in spans:
     plt.pcolormesh(df, edgecolor='black', linewidths=0.1, cmap='bwr', alpha=0.75)
     plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
 
-    xticks = [pos for pos in range(1, len(df.columns)+1) if pos %n[span] == 0]
-    labels = [df.columns[xtick-1] for xtick in xticks]
+    xticks = [pos for pos in range(1, len(df.columns) + 1) if pos % n[span] == 0]
+    labels = [df.columns[xtick - 1] for xtick in xticks]
     plt.xticks(xticks, labels)
 
     plt.title(span + 's')
-    plt.savefig('E:/image.png', dpi=300, bbox_inches="tight")
+    plt.savefig('/home/files/output/png/logreg/feature_selection_rfe_logreg_'+span+'s.png', dpi=300, bbox_inches="tight")
     plt.show()
     plt.clf()
-
-
-
-print("finish!")
