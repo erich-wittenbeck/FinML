a20fd437b08fe3fb71e698e861fdfb041d56d037
erich_wittenbeck <erich.wittenbeck@gmx.net>
2020-03-02 11:11:24 +0100

    - Adapted script for debugging

--- scripts/experiments/prediction/real_data_logreg_randf_stochastic_auc.py
+++ scripts/experiments/prediction/real_data_logreg_randf_stochastic_auc.py
@@ -53,7 +53,7 @@ for avg in ['micro']: # 'macro',
             test_data.standardize(training_data)
 
             logreg = Classification('logreg', 'logreg') \
-                .set_hyper_parameters(C=[10**exp for exp in range(-6,7)]) \
+                .set_hyper_parameters(C=[10**exp for exp in range(-6,4)]) \
                 .configure_hpo('exhaustive', 'f1_macro', n_jobs=6, verbose=2) \
                 .train(training_data, prune_features=True, rfe_scoring='f1_macro')
 
